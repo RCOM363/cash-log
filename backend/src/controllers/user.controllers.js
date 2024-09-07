45bc55995f28390bcaea0a34142fe439af0cd447
RCOM363 <rahullankeppanavar363@gmail.com>
2024-09-07 19:51:01 +0530

    update cookies settings

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -107,6 +107,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
   console.log("User logged in successfully");
   return res
